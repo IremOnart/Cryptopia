e070e8a695ae3205d9b292be288dd1ae4b0fae30
IremOnart <78277981+IremOnart@users.noreply.github.com>
2023-08-10 17:03:38 +0300

    line spaces is fixed

--- Cryptopia/Cryptopia/Cryptopia/Scenes/CryptoList/CryptoListViewController.swift
+++ Cryptopia/Cryptopia/Cryptopia/Scenes/CryptoList/CryptoListViewController.swift
@@ -28,26 +28,26 @@ class CryptoListViewController: UIViewController, UISearchBarDelegate {
         
         let nib = UINib(nibName: "CryptopiaTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "TableViewCell")
-
+        
         viewModel.delegate = self
         self.navigationItem.setHidesBackButton(true, animated: true)
         searchController.searchBar.searchTextField.backgroundColor = .clear
         initSearchController()
-
-       }
+        
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         viewModel.getData()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
-         super.viewWillAppear(animated)
-         clearNavigationBar(clear: true)
+        super.viewWillAppear(animated)
+        clearNavigationBar(clear: true)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-         super.viewWillDisappear(animated)
-         clearNavigationBar(clear: false)
+        super.viewWillDisappear(animated)
+        clearNavigationBar(clear: false)
     }
     
     
@@ -67,7 +67,7 @@ class CryptoListViewController: UIViewController, UISearchBarDelegate {
 }
 
 extension CryptoListViewController {
-
+    
     func clearNavigationBar(clear: Bool) {
         if clear {
             let appearance = UINavigationBarAppearance()
@@ -118,31 +118,31 @@ extension CryptoListViewController: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! CryptopiaTableViewCell
-
-            let inSearchMode = self.viewModel.inSearchModel(searchController)
-            let coin = inSearchMode ? self.viewModel.filteredCoins[indexPath.row] : self.viewModel.getCoin(for: indexPath)
-
-            cell.coinNameLabel.text = coin.name
-            cell.coinSymbolLabel.text = coin.symbol
-            cell.priceLabel.text = Double(round(10000 * (coin.price ?? 0))/10000).formatted()
-            cell.priceChangeLabel.text = "% \(Double(coin.priceChange1d ?? 0).formatted())"
-            cell.iconImageView.kf.setImage(with: URL(string: coin.icon ?? ""))
-            cell.priceChangeLabel.textColor = (coin.priceChange1d ?? 0.0) > 0 ? .green : (coin.priceChange1d ?? 0.0) < 0 ? .red : .black
-
-            return cell
+        
+        let inSearchMode = self.viewModel.inSearchModel(searchController)
+        let coin = inSearchMode ? self.viewModel.filteredCoins[indexPath.row] : self.viewModel.getCoin(for: indexPath)
+        
+        cell.coinNameLabel.text = coin.name
+        cell.coinSymbolLabel.text = coin.symbol
+        cell.priceLabel.text = Double(round(10000 * (coin.price ?? 0))/10000).formatted()
+        cell.priceChangeLabel.text = "% \(Double(coin.priceChange1d ?? 0).formatted())"
+        cell.iconImageView.kf.setImage(with: URL(string: coin.icon ?? ""))
+        cell.priceChangeLabel.textColor = (coin.priceChange1d ?? 0.0) > 0 ? .green : (coin.priceChange1d ?? 0.0) < 0 ? .red : .black
+        
+        return cell
         
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-    
+        
         let inSearchMode = self.viewModel.inSearchModel(searchController)
         let coin = inSearchMode ? self.viewModel.filteredCoins[indexPath.row] : self.viewModel.getCoin(for: indexPath)
         
         let vm = CryptopiaDetailViewModel(coin: coin)
         let vc = CryptopiaDetailViewController(vm)
         self.navigationController?.pushViewController(vc, animated: true)
-
+        
         
     }
     
